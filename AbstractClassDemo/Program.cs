d4ee1371a8746ad1bde85a85b0e3c05da9292660
Alvin Bernasko <alvinbernasko123@gmail.com>
2024-06-16 10:32:33 +0000

    update

---
+++ AbstractClassDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace AbstractClassDemo
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override double GetArea()
+        {
+            return width * height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape myCircle = new Circle(5);
+            Shape myRectangle = new Rectangle(4, 6);
+
+            Console.WriteLine($"Area of Circle: {myCircle.GetArea()}");
+            Console.WriteLine($"Area of Rectangle: {myRectangle.GetArea()}");
+        }
+    }
+}
